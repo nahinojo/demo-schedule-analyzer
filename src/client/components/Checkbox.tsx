beaebfddbc7f90011ba7715d35296d4a3c8d407d
Noah Hinojos <noah.hinojos@gmail.com>
2024-01-11 14:24:47 -0800

    fix: removed unused component

--- src/client/components/Checkbox.tsx
+++
@@ -1,9 +0,0 @@
-// import React, { useState } from 'react'
-// import { Checkbox } from '@mui/material'
-
-// import type { FC } from 'react'
-
-// export const CustomCheckbox: FC = () => {
-//   const [isOn, setIsOn] = useState<boolean>(false)
-
-// }
