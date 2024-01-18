ee56cf52e46082eefb5997136b78d869d14b239a
Noah Hinojos <noah.hinojos@gmail.com>
2024-01-18 09:44:54 -0800

    feat: init db writing

--- src/api/main_new.py
+++ src/api/main_new.py
@@ -1,22 +1,18 @@
-import os
-
-from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 
-load_dotenv()
-
 app = Flask(__name__)
-app.config['SQL_ALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', default=True)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable modification tracking
 db = SQLAlchemy(app)
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.string(80), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
 
     def __repr__(self):
-        return f'<User {self.username}'
+        return f'<User {self.username}>'
 
 
 @app.before_request
@@ -26,9 +22,20 @@ def create_tables():
 
 @app.route('/')
 def index():
+    # Example: query all users from the 'User' table
     users = User.query.all()
     return '\n'.join([user.username for user in users])
 
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    data = request.args
+    username = data.get('username')
+
+    new_user = User(username=username)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return f'User {username} added successfully'
 
 if __name__ == '__main__':
     app.run(debug=True)
