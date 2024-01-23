bbca25d0d403dad4409b8e5a978ede08cd22fc20
Noah Hinojos <noah.hinojos@gmail.com>
2024-01-23 15:13:06 -0800

    chore: abstracted app creation

--- src/api/run_test.py
+++ src/api/run_test.py
@@ -1,15 +1,7 @@
-from database import db
 from create_app import create_app
-
 import pytest
-
-app = create_app()
-
-
-@app.route('/test')
-def test():
-    return {"status": "success", "message": "Test of Flask Application successfully performed!"}, 200
 
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
