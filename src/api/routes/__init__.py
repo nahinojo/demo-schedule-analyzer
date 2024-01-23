943b3bbeaaca84cabc223b74a51f7ead4b6c0138
Noah Hinojos <noah.hinojos@gmail.com>
2024-01-23 14:11:16 -0800

    chore: simple refactoring

---
+++ src/api/routes/__init__.py
@@ -0,0 +1,4 @@
+# from .add_and_edit_course import add_and_edit_course
+# from .add_test_course import add_test_course
+# from .get_all_courses import get_all_courses
+from .routes import test_blueprint
---
+++ src/api/routes/routes.py
@@ -0,0 +1,7 @@
+from flask import Blueprint
+# from . import get_all_courses, add_test_course, add_and_edit_course
+
+test_blueprint = Blueprint('api', __name__)
+# test_blueprint.route('/add_and_edit_course', methods=['POST'])(add_and_edit_course)
+# test_blueprint.route('/add_test_course', methods=['POST'])(add_test_course)
+# test_blueprint.route('/get_all_courses', methods=['GET'])(get_all_courses)
