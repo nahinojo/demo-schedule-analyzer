9b725f1e0eb9a4b48ab69f451c03d86ebed11de8
Noah Hinojos <noah.hinojos@gmail.com>
2024-01-24 13:13:22 -0800

    chore: cleanup testing surrounding create_course_test.py

--- src/api/tests/create_course_test.py
+++ src/api/tests/create_course_test.py
@@ -21,7 +21,21 @@ def create_course_test():
         year=2024,
         demo_event=[demo_event]
     )
-
     db.session.add(course)
     db.session.commit()
-    assert 200
+    test_course = db.session.get(Course, 1)
+    test_course = test_course.serialize()
+    assert test_course == {
+        'id': 1,
+        'course_code': 'COURSE_CODE_TEST',
+        'instructor': 'INSTRUCTOR_TEST',
+        'term': 'TERM_TEST', 'year': 2024,
+        'demo_event': [
+            {'id': 1,
+             'date': date(2005, 1, 1),
+             'demos': [{'id': 1, 'name': 'DEMO_1_NAME_TEST'},
+                       {'id': 2, 'name': 'DEMO_2_NAME_TEST'}],
+             'additional_info': 'ADDITIONAL_INFO_TEST'}
+        ]
+    }
+
