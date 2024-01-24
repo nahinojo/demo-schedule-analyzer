5e82233e729297e636d6c92150e42071551bc407
Noah Hinojos <noah.hinojos@gmail.com>
2024-01-24 13:13:22 -0800

    chore: cleanup testing surrounding create_course_test.py

--- src/api/tests/conftest.py
+++ src/api/tests/conftest.py
@@ -9,22 +9,7 @@ def app():
     app.config.update({
         "TESTING": True,
     })
-
-    # other setup can go here
-
     yield app
-
-    # clean up / reset resources here
-
-
-@pytest.fixture()
-def client(app):
-    return app.test_client()
-
-
-@pytest.fixture()
-def runner(app):
-    return app.test_cli_runner()
 
 
 @pytest.fixture()
