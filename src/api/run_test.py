c9da7846e893ad007217243d6f88864b068031a9
Noah Hinojos <noah.hinojos@gmail.com>
2024-01-24 11:42:55 -0800

    feat: setup db testing

--- src/api/run_test.py
+++ src/api/run_test.py
@@ -1,8 +1,15 @@
-from create_app import create_app
+# from create_app import create_app
 import pytest
 
-# See https://flask.palletsprojects.com/en/3.0.x/testing/ and modify later.
-if __name__ == '__main__':
-    app = create_app()
-    app.testing = True
-    app.test_client()
+
+@pytest.fixture()
+def test_request_example(client):
+    response = client.get("/posts")
+    assert b"<h2>Hello, World!</h2>" in response.data
+
+
+# # See https://flask.palletsprojects.com/en/3.0.x/testing/ and modify later.
+# if __name__ == '__main__':
+#     app = create_app()
+#     app.testing = True
+#     app.test_client()
