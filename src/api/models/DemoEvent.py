6098a8ac6a556724136ee7b72bf3862e84d213fe
Noah Hinojos <noah.hinojos@gmail.com>
2024-01-24 15:19:23 -0800

    feat: setup model methods to add attributes (untested)

--- src/api/models/DemoEvent.py
+++ src/api/models/DemoEvent.py
@@ -1,12 +1,25 @@
 from database import db
+from models import Demo
 
 
 class DemoEvent(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    event_date = db.Column(db.Date, nullable=False)
-    demo = db.relationship("Demo", backref="demo_event", lazy=True)
+    event_date = db.Column(db.Date)
     additional_info = db.Column(db.Text)
-    course_id = db.Column(db.Integer, db.ForeignKey("course.id"), nullable=False)
+    demo = db.relationship("Demo", backref="demo_event")
+    course_id = db.Column(db.Integer, db.ForeignKey("course.id"))
+
+    def add_demo(
+            self,
+            name: str,
+    ):
+        demo = Demo(
+            name=name,
+            demo_event_id=self.demo_event_id
+        )
+        db.session.add(demo)
+        db.session.commit()
+        return demo
 
     def serialize(self):
         return {
