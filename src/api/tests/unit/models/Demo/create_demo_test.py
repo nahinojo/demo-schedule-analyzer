ae1ca7506b3a7b4371309f5a21aa414bd762bf54
Noah Hinojos <noah.hinojos@gmail.com>
2024-01-26 08:08:10 -0800

    chore: cleanup

--- src/api/tests/unit/models/Demo/create_demo_test.py
+++ src/api/tests/unit/models/Demo/create_demo_test.py
@@ -6,10 +6,9 @@ import pytest
 
 @pytest.mark.usefixtures("app_context_with_test_course")
 def create_demo_test():
-    test_demo = Demo(name="CREATE_DEMO_TEST")
+    test_demo = Demo(name="CREATE_DEMO_NAME_TEST")
     test_course = db.session.get(Course, 1)
-    print(test_course.demo_events[0].serialize())
     test_course.demo_events[0].demos = [test_demo]
     db.session.commit()
-    assert test_course.demo_events[0].demos[0].name == "CREATE_DEMO_TEST"
+    assert test_course.demo_events[0].demos[0].name == "CREATE_DEMO_NAME_TEST"
     return
