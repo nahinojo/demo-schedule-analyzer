113d23875c51a878b823f2d435b042539545f3f6
Noah Hinojos <noah.hinojos@gmail.com>
2024-03-12 12:44:09 -0700

    chore: cleanup

--- src/api/app/database/setup_db.py
+++ src/api/app/database/setup_db.py
@@ -11,7 +11,7 @@ def setup_db():
         for table in reversed(Base.metadata.sorted_tables):
             session.execute(table.delete())
         all_courses = extract_courses_from_calendar(
-            target_year= 2018
+            target_year=2018
         )
         for course in all_courses:
             session.add(course)
