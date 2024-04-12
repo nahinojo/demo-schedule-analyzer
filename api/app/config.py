3c6c3f7f5fb6d14e22eb97a403580262da4767f9
Noah Hinojos <noah.hinojos@gmail.com>
2024-04-11 18:00:46 -0700

    fix: ensure proper path for local execution

--- api/app/config.py
+++ api/app/config.py
@@ -4,19 +4,15 @@ import os
 class Config:
     DEBUG = False
     TESTING = False
-    # DATABASE_PATH differs depending on running in Docker vs CLI.
-    SQLALCHEMY_DATABASE_PATH = f"{os.getcwd()}/app/database/main.db"
+    # Database path differs between docker container and local development
+    CWD = os.getcwd()
+    if "api" not in CWD[-4:]:
+        if CWD[-1] == "/":
+            SQLALCHEMY_DATABASE_PATH = f"{CWD}api/app/database/main.db"
+        else:
+            SQLALCHEMY_DATABASE_PATH = f"{CWD}/api/app/database/main.db"
+    else:
+        SQLALCHEMY_DATABASE_PATH = f"{CWD}/app/database/main.db"
     SQLALCHEMY_DATABASE_URI = f"sqlite:////{SQLALCHEMY_DATABASE_PATH}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-
-
-class TestingConfig(Config):
-    DEBUG = True
-    TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
