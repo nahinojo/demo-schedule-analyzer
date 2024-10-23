b9e59b6af7c02cae91672b83d62f893b1e4b67ae
Noah Hinojos <noah.hinojos@gmail.com>
2024-10-23 10:39:59 -0700

    chore: documented tests

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -3,12 +3,12 @@ Fixtures for testing.
 """
 import pytest
 from flask import Flask
-from datetime import datetime
 from sqlalchemy.orm import Session
 
 from app import create_app
-from app.models import Demo, DemoEvent, Course
 from app.database import Database
+from app.models import Course, Demo, DemoEvent
+from tests.utils import random_course, random_demo_event, random_demo
 
 
 # App
@@ -16,6 +16,16 @@ from app.database import Database
 def app(monkeypatch: pytest.MonkeyPatch) -> Flask:
     """
     Flask app fixture for testing.
+
+    Parameters
+    ----------
+    monkeypatch: pytest.MonkeyPatch
+        The monkeypatch fixture for configuring the environment.
+
+    Returns
+    -------
+    Flask
+        The generated Flask app.
     """
     monkeypatch.setenv("APP_ENV", "testing")
     app = create_app()
@@ -27,6 +37,16 @@ def app(monkeypatch: pytest.MonkeyPatch) -> Flask:
 def db_session(app: Flask) -> Session:
     """
     Database session.
+
+    Parameters
+    ----------
+    app: Flask
+        The Flask app fixture.
+
+    Returns
+    -------
+    Session
+        The database session.
     """
     with app.app_context():
         _session = Database.get_session()
@@ -38,33 +58,47 @@ def db_session(app: Flask) -> Session:
 @pytest.fixture()
 def demo() -> Demo:
     """
-    Tests the creation of the demo model.
+    Demo fixture that can be used in testing.
+
+    Returns
+    -------
+    Demo
+        The generated demo.
     """
-    yield Demo(name="DEMO_TEST")
+    yield random_demo()
 
 
 @pytest.fixture
 def demo_event(demo) -> DemoEvent:
     """
-    Tests the creation of the demo event model.
+    DemoEvent fixture that can be used in testing.
+
+    Parameters
+    ----------
+    demo: Demo
+        The demo fixture to be associated with the demo event.
+
+    Returns
+    -------
+    DemoEvent
+        The generated demo event.
     """
-    today = datetime.now().date()
-    yield DemoEvent(
-        event_date=today,
-        additional_information="ADDITIONAL_INFORMATION_TEST",
-        demos=[demo]
-        )
+    yield random_demo_event(demos=[demo])
 
 
 @pytest.fixture
 def course(demo_event) -> Course:
     """
-    Tests the creation of the course model.
+    Course fixture that can be used in testing.
+
+    Parameters
+    ----------
+    demo_event: DemoEvent
+        The demo_event fixture to be associated with the course.
+
+    Returns
+    -------
+    Course
+        The generated course.
     """
-    yield Course(
-        course_code="COURSE_CODE_TEST",
-        instructor="INSTRUCTOR_TEST",
-        term="TERM_TEST",
-        year=datetime.now().year,
-        demo_events=[demo_event]
-        )
+    yield random_course(demo_events=[demo_event])
--- api/tests/new_course_test.py
+++ api/tests/new_course_test.py
@@ -7,30 +7,60 @@ from datetime import datetime
 from flask import Flask
 from sqlalchemy.orm import Session
 
-from app.models import DemoEvent, Course
+from app.models import Course, DemoEvent, Demo
 from app.daos import CourseDAO
 
 
 def create_course_test(app: Flask,
                        db_session: Session,
                        course: Course,
-                       demo_event: DemoEvent
+                       demo_event: DemoEvent,
+                       demo: Demo
                        ) -> None:
     """
-    Tests the creation of the Course model, and associated child models.
+    Tests the creation of the Course model and associated child models.
+
+    Parameters
+    ----------
+    app: Flask
+        The Flask app.
+    db_session: Session
+        The database session.
+    course: Course
+        The course fixture to be used for testing.
+    demo_event: DemoEvent
+        The demo_event fixture to be used for testing.
+    demo: Demo
+        The demo fixture to be used for testing.
+
+    Returns
+    -------
+    None
     """
-    assert course.course_code == "COURSE_CODE_TEST"
-    assert course.instructor == "INSTRUCTOR_TEST"
-    assert course.term == "TERM_TEST"
-    assert course.year == datetime.now().year
-    assert course.demo_events == [demo_event]
+    assert course.course_code
+    assert course.instructor
+    assert course.term
+    assert course.year
+    assert course.demo_events[0] == demo_event
+    assert demo_event.demos[0] == demo
 
 
 def add_course_to_db_test(db_session: Session,
                           course: Course
                           ) -> None:
     """
-    Adds a course to the database.
+    Tests adding a course to the database.
+
+    Parameters
+    ----------
+    db_session: Session
+        The database session.
+    course: Course
+        The course fixture to be used for testing.
+
+    Returns
+    -------
+    None
     """
     with db_session.begin():
         course_dao = CourseDAO(db_session)
